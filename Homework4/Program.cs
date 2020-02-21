846ac34e0d9d475e9612b34ac128b64c0409db06
delia baletchi <delia.micaela@yahoo.com>
2020-02-22 00:52:54 +0200

    Add project files.

---
+++ Homework4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Problema 1
+
+            Animal cutu = new Animal("caine", "negru", 2);
+            Animal gaina = new Animal("gaine", "alba", 22);
+            Animal tigru = new Animal("tigru", "galben", 7);
+
+            Imobil apartament = new Imobil("bloc");
+            apartament.nrEtaje = 10;
+            apartament.pret = 2222.2;
+
+
+
+        }
+    }
+}
